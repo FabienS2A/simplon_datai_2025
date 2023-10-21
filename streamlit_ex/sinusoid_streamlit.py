c99d95c6a2003cad0a576a23fc59e4a1297fbb91
Fabien Soulavie <fabien.soulave@gmail.com>
2023-10-21 13:20:51 +0200

    update

---
+++ streamlit_ex/sinusoid_streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+
+freq = st.slider('Select frequency', 0.0, 50.0, 25.0)
+
+t = np.linspace(0.1, 1.0, num = 10000)
+
+
+x = (2*np.pi*freq*t)   # start,stop,step
+y = np.sin(x)
+st.line_chart(y)
--- streamlit_ex/streamlit_test.py
+++ streamlit_ex/streamlit_test.py
@@ -26,16 +26,3 @@ agree = st.checkbox('Do you want see more ?')
 if agree:
     st.write('Not today')
 
-freq = st.slider('Select frequency', 0.0, 50.0, 25.0)
-
-t = np.linspace(0.1, 1.0, num = 10000)
-
-#for i in range(10):
-  #x = np.arange(i,i+2*np.pi,0.1)   # start,stop,step
-  #y = np.sin(x)
-  #st.line_chart(y)
-
-
-x = (2*np.pi*freq*t)   # start,stop,step
-y = np.sin(x)
-st.line_chart(y)
